6d376f11490f742c45dd0c90d706624bf05c33fb
io-developer <i.o.developer@gmail.com>
2020-06-21 02:29:25 +0300

    feat: decribe metrics, rename, add new

--- collect/metrics.go
+++ collect/metrics.go
@@ -49,17 +49,23 @@ NOMBATTV : 12.0 Volts
 ITEMP    : 29.2 C
 BATTDATE : 2018-11-29
 MANDATE  : 2018-01-09
-
-DWAKE    : 0 Seconds
 DSHUTD   : 180 Seconds
+DWAKE    : 0 Seconds
+**DLOWBATT**
+    The remaining runtime below which the UPS
+    sends the low battery signal. At this point apcupsd will force an
+	immediate emergency shutdown.
+NUMXFERS : 0
 RETPCT   : 0.0 Percent
+
+
+
 ALARMDEL : No alarm|30 Seconds
 
 LASTXFER : Unacceptable line voltage changes
 | No transfers since turnon
 | Automatic or explicit self test
 
-NUMXFERS : 0
 
 SELFTEST : NO
 STESTI   : None|14 days
@@ -67,189 +73,297 @@ STATFLAG : 0x05000008
 SERIALNO : 4B1802P05216
 FIRMWARE : 808.q10 .I USB FW:q
 
+**HUMIDITY**
+	The humidity as measured by the UPS.
 
+**AMBTEMP**
+    The ambient temperature as measured by the UPS.
+
+**EXTBATTS**
+    The number of external batteries as
+    defined by the user. A correct number here helps the UPS compute
+    the remaining runtime more accurately.
+
+**BADBATTS**
+    The number of bad battery packs.
 */
 
 // Metrics declare
 var Metrics = []*Metric{
 
-	// Battery
-	{
-		Gauge: prometheus.NewGauge(prometheus.GaugeOpts{
-			Name: "apcupsd_battery_charge",
-			Help: "Current battery charge (percent)",
-		}),
-		OutputKey: "BCHARGE",
-	},
-	{
-		Gauge: prometheus.NewGauge(prometheus.GaugeOpts{
-			Name: "apcupsd_battery_voltage",
-			Help: "",
-		}),
-		OutputKey: "BATTV",
-	},
-	{
-		Gauge: prometheus.NewGauge(prometheus.GaugeOpts{
-			Name: "apcupsd_battery_voltage_nominal",
-			Help: "",
-		}),
-		OutputKey: "NOMBATTV",
-	},
-	{
-		Gauge: prometheus.NewGauge(prometheus.GaugeOpts{
-			Name: "apcupsd_battery_datetime",
-			Help: "ts",
-		}),
-		OutputKey: "BATTDATE",
-		Type:      "date",
-	},
-
 	// Input
 	{
 		Gauge: prometheus.NewGauge(prometheus.GaugeOpts{
 			Name: "apcupsd_input_sensitivity",
-			Help: "",
+			Help: "**SENSE** The sensitivity level of the UPS to line voltage fluctuations.\n Unknown=0, Low=1, Medium=2, High=3, 'Auto Adjust'=4",
 		}),
 		OutputKey: "SENSE",
 		Type:      "valueMap",
 		ValueMap: map[string]float64{
-			"Low":    0,
-			"Normal": 1,
-			"High":   2,
+			"Unknown":     0,
+			"Low":         1,
+			"Medium":      2,
+			"High":        3,
+			"Auto Adjust": 4,
 		},
 	},
 	{
 		Gauge: prometheus.NewGauge(prometheus.GaugeOpts{
+			Name: "apcupsd_input_frequency",
+			Help: "**LINEFREQ** Line frequency in hertz as given by the UPS.",
+		}),
+		OutputKey: "LINEFREQ",
+	},
+	{
+		Gauge: prometheus.NewGauge(prometheus.GaugeOpts{
 			Name: "apcupsd_input_voltage",
-			Help: "",
+			Help: "**LINEV** The current line voltage as returned by the UPS.",
 		}),
 		OutputKey: "LINEV",
 	},
 	{
 		Gauge: prometheus.NewGauge(prometheus.GaugeOpts{
+			Name: "apcupsd_input_voltage_min",
+			Help: "**MINLINEV** The minimum line voltage since the UPS was started, as returned by the UPS",
+		}),
+		OutputKey: "MINLINEV",
+	},
+	{
+		Gauge: prometheus.NewGauge(prometheus.GaugeOpts{
+			Name: "apcupsd_input_voltage_max",
+			Help: "**MAXLINEV** The maximum line voltage since the UPS was started, as reported by the UPS",
+		}),
+		OutputKey: "MAXLINEV",
+	},
+	{
+		Gauge: prometheus.NewGauge(prometheus.GaugeOpts{
 			Name: "apcupsd_input_voltage_nominal",
-			Help: "",
+			Help: "**NOMINV** The input voltage that the UPS is configured to expect.",
 		}),
 		OutputKey: "NOMINV",
 	},
 	{
 		Gauge: prometheus.NewGauge(prometheus.GaugeOpts{
-			Name: "apcupsd_input_frequency",
-			Help: "",
+			Name: "apcupsd_input_voltage_transfer_low",
+			Help: "**LOTRANS** The line voltage below which the UPS will switch to batteries.",
 		}),
-		OutputKey: "LINEFREQ",
+		OutputKey: "LOTRANS",
+	},
+	{
+		Gauge: prometheus.NewGauge(prometheus.GaugeOpts{
+			Name: "apcupsd_input_voltage_transfer_high",
+			Help: "**HITRANS** The line voltage above which the UPS will switch to batteries.",
+		}),
+		OutputKey: "HITRANS",
 	},
 
 	// Output
 	{
 		Gauge: prometheus.NewGauge(prometheus.GaugeOpts{
 			Name: "apcupsd_output_load",
-			Help: "",
+			Help: "**LOADPCT** The percentage of load capacity as estimated by the UPS.",
 		}),
 		OutputKey: "LOADPCT",
 	},
 	{
 		Gauge: prometheus.NewGauge(prometheus.GaugeOpts{
 			Name: "apcupsd_output_power_nominal",
-			Help: "",
+			Help: "**NOMPOWER** The maximum power in Watts that the UPS is designed to supply.",
 		}),
 		OutputKey: "NOMPOWER",
 	},
 	{
 		Gauge: prometheus.NewGauge(prometheus.GaugeOpts{
 			Name: "apcupsd_output_voltage",
-			Help: "",
+			Help: "**OUTPUTV** The voltage the UPS is supplying to your equipment",
 		}),
 		OutputKey: "OUTPUTV",
 	},
 	{
 		Gauge: prometheus.NewGauge(prometheus.GaugeOpts{
 			Name: "apcupsd_output_voltage_nominal",
-			Help: "",
+			Help: "**NOMOUTV** The output voltage that the UPS will attempt to supply when on battery power.",
 		}),
 		OutputKey: "NOMOUTV",
+	},
+
+	// Battery
+	{
+		Gauge: prometheus.NewGauge(prometheus.GaugeOpts{
+			Name: "apcupsd_battery_charge",
+			Help: "**BCHARGE** The percentage charge on the batteries.",
+		}),
+		OutputKey: "BCHARGE",
+	},
+	{
+		Gauge: prometheus.NewGauge(prometheus.GaugeOpts{
+			Name: "apcupsd_battery_voltage",
+			Help: "**BATTV** Battery voltage as supplied by the UPS.",
+		}),
+		OutputKey: "BATTV",
+	},
+	{
+		Gauge: prometheus.NewGauge(prometheus.GaugeOpts{
+			Name: "apcupsd_battery_voltage_nominal",
+			Help: "**NOMBATTV** The nominal battery voltage.",
+		}),
+		OutputKey: "NOMBATTV",
+	},
+	{
+		Gauge: prometheus.NewGauge(prometheus.GaugeOpts{
+			Name: "apcupsd_battery_replaced_timestamp",
+			Help: "**BATTDATE** The date that batteries were last replaced.",
+		}),
+		OutputKey: "BATTDATE",
+		Type:      "date",
 	},
 
 	// Ups
 	{
 		Gauge: prometheus.NewGauge(prometheus.GaugeOpts{
-			Name: "apcupsd_ups_manafactured_datetime",
-			Help: "ts",
+			Name: "apcupsd_ups_manafactured_timestamp",
+			Help: "**MANDATE** The date the UPS was manufactured.",
 		}),
 		OutputKey: "MANDATE",
 		Type:      "date",
 	},
 	{
 		Gauge: prometheus.NewGauge(prometheus.GaugeOpts{
+			Name: "apcupsd_ups_status_flag",
+			Help: "**STATFLAG** Status flag. English version is given by STATUS.",
+		}),
+		OutputKey: "STATFLAG",
+	},
+	{
+		Gauge: prometheus.NewGauge(prometheus.GaugeOpts{
+			Name: "apcupsd_ups_dip_switch_flag",
+			Help: "**DIPSW** The current dip switch settings on UPSes that have them.",
+		}),
+		OutputKey: "DIPSW",
+	},
+	{
+		Gauge: prometheus.NewGauge(prometheus.GaugeOpts{
+			Name: "apcupsd_ups_reg1",
+			Help: "**REG1** The value from the UPS fault register 1.",
+		}),
+		OutputKey: "REG1",
+	},
+	{
+		Gauge: prometheus.NewGauge(prometheus.GaugeOpts{
+			Name: "apcupsd_ups_reg2",
+			Help: "**REG2** The value from the UPS fault register 2.",
+		}),
+		OutputKey: "REG2",
+	},
+	{
+		Gauge: prometheus.NewGauge(prometheus.GaugeOpts{
+			Name: "apcupsd_ups_reg3",
+			Help: "**REG3** The value from the UPS fault register 3.",
+		}),
+		OutputKey: "REG3",
+	},
+
+	{
+		Gauge: prometheus.NewGauge(prometheus.GaugeOpts{
 			Name: "apcupsd_ups_temp_internal",
-			Help: "",
+			Help: "**ITEMP** (Celsius) Internal UPS temperature as supplied by the UPS.",
 		}),
 		OutputKey: "ITEMP",
 	},
 	{
 		Gauge: prometheus.NewGauge(prometheus.GaugeOpts{
-			Name: "apcupsd_ups_transfer_voltage_low",
-			Help: "",
-		}),
-		OutputKey: "LOTRANS",
-	},
-	{
-		Gauge: prometheus.NewGauge(prometheus.GaugeOpts{
-			Name: "apcupsd_ups_transfer_voltage_high",
-			Help: "",
-		}),
-		OutputKey: "HITRANS",
-	},
-	{
-		Gauge: prometheus.NewGauge(prometheus.GaugeOpts{
 			Name: "apcupsd_ups_timeleft",
-			Help: "seconds",
+			Help: "**TIMELEFT** (seconds) The remaining runtime left on batteries as estimated by the UPS.",
 		}),
 		OutputKey: "TIMELEFT",
 		Type:      "minutes",
 	},
 	{
 		Gauge: prometheus.NewGauge(prometheus.GaugeOpts{
-			Name: "apcupsd_ups_onbattery",
-			Help: "seconds",
+			Name: "apcupsd_ups_timeleft_low_battery",
+			Help: "**DLOWBATT** (seconds) The remaining runtime below which the UPS sends the low battery signal. At this point apcupsd will force an immediate emergency shutdown.",
+		}),
+		OutputKey: "DLOWBATT",
+		Type:      "minutes",
+	},
+	{
+		Gauge: prometheus.NewGauge(prometheus.GaugeOpts{
+			Name: "apcupsd_ups_transfer_onbattery_count",
+			Help: "**NUMXFERS** The number of transfers to batteries since apcupsd startup.",
+		}),
+		OutputKey: "NUMXFERS",
+	},
+	{
+		Gauge: prometheus.NewGauge(prometheus.GaugeOpts{
+			Name: "apcupsd_ups_transfer_onbattery_time",
+			Help: "**TONBATT** Time in seconds currently on batteries",
 		}),
 		OutputKey: "TONBATT",
 	},
 	{
 		Gauge: prometheus.NewGauge(prometheus.GaugeOpts{
-			Name: "apcupsd_ups_onbattery_cumulative",
-			Help: "seconds",
+			Name: "apcupsd_ups_transfer_onbattery_time_cumulative",
+			Help: "Total (cumulative) time on batteries in seconds since apcupsd startup.",
 		}),
 		OutputKey: "CUMONBATT",
 	},
 	{
 		Gauge: prometheus.NewGauge(prometheus.GaugeOpts{
-			Name: "apcupsd_ups_offbattery_datetime",
-			Help: "ts",
+			Name: "apcupsd_ups_transfer_onbattery_timestamp",
+			Help: "**XONBATT** Time and date of last transfer to batteries",
 		}),
-		OutputKey: "CUMONBATT",
+		OutputKey: "XONBATT",
+	},
+	{
+		Gauge: prometheus.NewGauge(prometheus.GaugeOpts{
+			Name: "apcupsd_ups_transfer_offbattery_timestamp",
+			Help: "Time and date of last transfer from batteries",
+		}),
+		OutputKey: "XOFFBATT",
+	},
+	{
+		Gauge: prometheus.NewGauge(prometheus.GaugeOpts{
+			Name: "apcupsd_ups_turnon_delay",
+			Help: "**DWAKE** (seconds) The amount of time the UPS will wait before restoring power to your equipment after a power off condition when the power is restored.",
+		}),
+		OutputKey: "DWAKE",
+	},
+	{
+		Gauge: prometheus.NewGauge(prometheus.GaugeOpts{
+			Name: "apcupsd_ups_turnon_battery_min",
+			Help: "	**RETPCT** The percentage charge that the batteries must have after a power off condition before the UPS will restore power to your equipment.",
+		}),
+		OutputKey: "RETPCT",
+	},
+
+	{
+		Gauge: prometheus.NewGauge(prometheus.GaugeOpts{
+			Name: "apcupsd_ups_turnoff_delay",
+			Help: "**DSHUTD** (seconds) The grace delay that the UPS gives after receiving a power down command from apcupsd before it powers off your equipment.",
+		}),
+		OutputKey: "DSHUTD",
 	},
 
 	// Shutdown
 	{
 		Gauge: prometheus.NewGauge(prometheus.GaugeOpts{
-			Name: "apcupsd_shutdown_min_charge",
-			Help: "",
+			Name: "apcupsd_shutdown_battery_min",
+			Help: "**MBATTCHG** If the battery charge percentage (BCHARGE) drops below this value, apcupsd will  shutdown your system.",
 		}),
 		OutputKey: "MBATTCHG",
 	},
 	{
 		Gauge: prometheus.NewGauge(prometheus.GaugeOpts{
-			Name: "apcupsd_shutdown_min_timeleft",
-			Help: "seconds",
+			Name: "apcupsd_shutdown_timeleft_min",
+			Help: "**MINTIMEL** (seconds) apcupsd will shutdown your system if the remaining runtime equals or is below this point.",
 		}),
 		OutputKey: "MINTIMEL",
 		Type:      "minutes",
 	},
 	{
 		Gauge: prometheus.NewGauge(prometheus.GaugeOpts{
-			Name: "apcupsd_shutdown_max_time",
-			Help: "seconds",
+			Name: "apcupsd_shutdown_onbattery_time_max",
+			Help: "**MAXTIME** (seconds) apcupsd will shutdown your system if the time on batteries exceeds this value. A value of zero disables the feature.",
 		}),
 		OutputKey: "MAXTIME",
 		Type:      "minutes",
