2fa0831f1bd4bb0818a01401defec07f26bc2de9
io-developer <i.o.developer@gmail.com>
2020-06-22 19:00:40 +0300

    refact: move server code to main

--- main.go
+++ main.go
@@ -2,14 +2,26 @@ package main
 
 import (
 	"flag"
-	"local/apcupsd_exporter/server"
+	"local/apcupsd_exporter/metric"
+	"net/http"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	promLog "github.com/prometheus/common/log"
 )
 
 func main() {
-	listenArg := flag.String("listen", "0.0.0.0:8001", "ip:port")
-	upsArg := flag.String("ups", "127.0.0.1:3551", "apcupsd host:port")
-	apcaccessArg := flag.String("apcaccess", "/sbin/apcaccess", "apcaccess path")
+	listenAddr := flag.String("listen", "0.0.0.0:8001", "ip:port")
+	upsAddr := flag.String("ups", "127.0.0.1:3551", "apcupsd host:port")
+	apcaccessPath := flag.String("apcaccess", "/sbin/apcaccess", "apcaccess path")
 	flag.Parse()
 
-	server.ListenAndServe(*listenArg, *upsArg, *apcaccessArg)
+	metric.RegisterPermanents()
+	go metric.CollectLoop(*apcaccessPath, *upsAddr, 5*time.Second)
+
+	promLog.Infoln("Starting exporter at", *listenAddr)
+	promLog.Infoln("Watching ups", *upsAddr)
+
+	http.Handle("/metrics", promhttp.Handler())
+	http.ListenAndServe(*listenAddr, nil)
 }
