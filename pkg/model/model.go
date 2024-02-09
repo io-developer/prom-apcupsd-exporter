90127b6432b09c3721497380329773d4f939a154
io-developer <i.o.developer@gmail.com>
2024-02-09 22:43:30 +0300

    del unused events

--- pkg/model/model.go
+++ pkg/model/model.go
@@ -6,11 +6,8 @@ const defaultEventLimit = 30
 type Model struct {
 	State         State
 	PrevState     State
-	events        []Event
-	NewEvents     []Event
 	EventLimit    int
 	ChangedFields map[string][]interface{}
-	onChange      []func(*Model)
 }
 
 // NewModel ..
@@ -19,26 +16,8 @@ func NewModel() *Model {
 		State:         NewState(),
 		PrevState:     NewState(),
 		ChangedFields: map[string][]interface{}{},
-		events:        []Event{},
-		NewEvents:     []Event{},
 		EventLimit:    defaultEventLimit,
-		onChange:      []func(*Model){},
 	}
-}
-
-// AddOnChange ..
-func (m *Model) AddOnChange(handler func(*Model)) {
-	m.onChange = append(m.onChange, handler)
-}
-
-// GetEvents ..
-func (m *Model) GetEvents() []Event {
-	return m.events
-}
-
-// AddEvent ..
-func (m *Model) AddEvent(e Event) {
-	m.NewEvents = append(m.NewEvents, e)
 }
 
 // Update method
@@ -48,18 +27,9 @@ func (m *Model) Update(newState State) {
 
 	m.updateStatusCounts()
 	m.updateTransferOnbatt()
-	m.updateEvents()
 
 	_, diff := m.PrevState.Compare(newState)
 	m.ChangedFields = diff
-
-	if len(diff) > 0 || len(m.NewEvents) > 0 {
-		for _, f := range m.onChange {
-			f(m)
-		}
-	}
-
-	m.trimEvents()
 }
 
 // updateStatusCounts method
@@ -94,27 +64,6 @@ func (m *Model) updateTransferOnbatt() {
 		}
 		if flag&StatusFlags["onbatt"] == 0 {
 			m.State.UpsStatus.FlagChangeCounts["onbatt"] += 2
-
-			m.AddEvent(eventFromType(EventTypeOnbatt, m.PrevState, m.State))
-			m.AddEvent(eventFromType(EventTypeOnbattEnd, m.PrevState, m.State))
 		}
 	}
-}
-
-// updateEvents method
-func (m *Model) updateEvents() {
-	events := eventsFromStateChanges(m.PrevState, m.State)
-	m.NewEvents = append(m.NewEvents, events...)
-}
-
-// trimEvents method
-func (m *Model) trimEvents() {
-	if len(m.NewEvents) == 0 {
-		return
-	}
-	m.events = append(m.events, m.NewEvents...)
-	if len(m.events) > m.EventLimit {
-		copy(m.events, m.events[len(m.events)-m.EventLimit:])
-	}
-	m.NewEvents = []Event{}
 }
